f00e724b0095997b87c90cd60724a908d1b2182a
ilyas <ilyashaider123@gmail.com>
2019-08-21 16:59:49 +0300

    added all folder

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\muhammad.ilyas\Cucumbess\BasicFrameWork
+mvn clean install
--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/FeaturesFiles",
-        glue = "stepDefinition",
-        plugin = {"pretty","html:test-output"},
-        monochrome = true,
-        dryRun = false,
-        strict = true
+        features = "src/test/FeaturesFiles/LoginWebPage.feature",
+        glue = "stepDefinition"
+       // plugin = {"pretty","html:test-output"},
+       // monochrome = true,
+       // dryRun = false,
+       // strict = true
        // tags = {"@EndtoEND, @Regression, @smoketest, @sanityTest"}
 )
 public class TestRunner {
